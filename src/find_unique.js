6e0d9e4da6f466b6d13a4abcb5b09d9fdb1dfd9a
ishakir <imran.pshakir@gmail.com>
2015-05-09 14:59:25 +0100

    Make findUnique only filter out if the original song didn't have that type

--- src/find_unique.js
+++ src/find_unique.js
@@ -1,7 +1,19 @@
-var getUnique = function(lastFmSong, spotifySearchResults) {
-	var potentialTracks = spotifySearchResults.filter(function(spotifySong) {
-		var name = spotifySong.name.toLowerCase();
-		return name.indexOf("live") < 0 && name.indexOf("remix") < 0 && name.indexOf("radio edit") < 0;
-	});
-	var chosenTrack = potentialTracks[0];
-}
+var findUnique = function(lastFmSong, spotifySearchResults) {
+	var contains = function(string, phrase) { return string.toLowerCase().indexOf(phrase.toLowerCase()) > -1; }
+	var lastFmContainsOrFilter = function(phrase) {
+		return function(song) {
+			return contains(lastFmSong.name, phrase) || !contains(song.name, phrase);
+		};
+	};
+	var filters = [
+		lastFmContainsOrFilter("live"),
+		lastFmContainsOrFilter("remix"),
+		lastFmContainsOrFilter("radio edit")
+	];
+	var filteredTracks = filters.reduce(function(currentFiltered, filter) {
+		return currentFiltered.filter(filter);
+	}, spotifySearchResults);
+	return filteredTracks[0];
+};
+
+module.exports.findUnique = findUnique
--- test/test_find_unique.js
+++ test/test_find_unique.js
@@ -1,13 +1,25 @@
-var _ = require('find_unique');
+var _ = require('../src/find_unique');
 var assert = require('assert');
 
-describe("The find unique method") {
-	it("should reject a remix if the original song entered wasn't a remix") {
-		var song = {name: "Trick of the Tail"};
-		var songs = [
-			{name: "Trick of the Tail", id: 1}
-			{name: "Trick of the Tail - Awesome Remix", id: 2}
-		];
-		assert.equal()
-	}
-}
+describe("The find unique method", function() {
+	var types = ["live", "remix", "radio edit"]
+
+	types.forEach(function(type) {
+		it("should reject a " + type + " version if the original song entered wasn't a " + type + " version", function() {
+			var song = {name: "Trick of the Tail"};
+			var songs = [
+				{name: "Trick of the Tail", id: 1},
+				{name: "Trick of the Tail - " + type, id: 2}
+			];
+			assert.equal(_.findUnique(song, songs).id, 1);
+		});	
+	});
+
+	types.forEach(function(type) {
+		it("should return a " + type + " version if the origin song entered was a " + type + " version", function() {
+			var song = {name: "Trick of the Tail - " + type};
+			var songs = [{name: "Trick of the Tail - " + type, id: 1}];
+			assert.equal(_.findUnique(song, songs).id, 1)
+		});
+	});
+});
